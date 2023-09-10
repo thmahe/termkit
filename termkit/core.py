22f3164fee694292808595e3906788827d6495a8
Thomas Mahé <contact@tmahe.dev>
2023-09-10 16:18:50 +0200

    Termkit: @command decorator added with optional custom name

--- termkit/core.py
+++ termkit/core.py
@@ -11,18 +11,45 @@ import sys
 import typing
 
 
+class _Command:
+    def __init__(self, callback: typing.Callable, name: str = None):
+        self.callback = callback
+        if name is None:
+            name = callback.__name__
+        self.name = name
+
+
 class Termkit:
     def __init__(self, name: str = os.path.basename(sys.argv[0])):
         self._childs = []
         self.name = name
 
-    def add(self, app_or_command: typing.Union[Termkit, typing.Callable]):
+    @property
+    def _child_names(self):
+        return [c.name for c in self._childs]
+
+    def add(self, app_or_command: typing.Union[Termkit, typing.Callable], name: typing.Optional[str] = None):
         if isinstance(app_or_command, Termkit):
-            self._childs.append(app_or_command)
+            child = app_or_command
         elif inspect.isfunction(app_or_command):
-            self._childs.append(app_or_command)
+            child = _Command(app_or_command, name)
         else:
-            raise TypeError(f"Cannot add object of type {type(app_or_command)} to termkit.Termkit.")
+            raise TypeError(f"Cannot add object of type '{type(app_or_command)}' to Termkit application.")
+
+        if name is not None:
+            child.name = name
+
+        if child.name in self._child_names:
+            raise ValueError(f"Name '{child.name}' already taken in '{self.name}' application.")
+
+        self._childs.append(child)
+
+    def command(self, name: str = None):
+        def decorator(func: typing.Callable):
+            self.add(func, name)
+            return func
+
+        return decorator
 
     def __call__(self, *args, **kwargs):
         print("Hello World")
--- tests/test_core.py
+++ tests/test_core.py
@@ -20,14 +20,35 @@ class TestCore(TestCase):
 
         self.assertEqual("Hello World\n", stdout.getvalue())
 
-    def test_add(self):
-        def dummy_command():
+    def test_add_termkit_sub_app(self):
+        app = Termkit()
+        app2 = Termkit()
+        app.add(app2)
+
+    def test_add_command(self):
+        app = Termkit("app-name")
+
+        def command():
             ...
 
+        app.add(command)
+        app.add(command, name="command-2")
+
+        with self.assertRaises(ValueError) as e:
+            app.add(command, name="command-2")
+        self.assertEqual(("Name 'command-2' already taken in 'app-name' application.",), e.exception.args)
+
+    def test_add_termkit_command_with_decorator(self):
+        app = Termkit("app-name")
+
+        @app.command()
+        def func_name():
+            ...
+
+        self.assertEqual(["func_name"], app._child_names)
+
+    def test_add_command_with_wrong_type(self):
         app = Termkit()
-        sub_app = Termkit()
-        app.add(sub_app)
-        app.add(dummy_command)
 
         class Complex:
             ...
@@ -35,6 +56,6 @@ class TestCore(TestCase):
         with self.assertRaises(TypeError) as e:
             app.add(Complex)
         self.assertEqual(
-            ("Cannot add object of type <class 'type'> to termkit.Termkit.",),
+            ("Cannot add object of type '<class 'type'>' to Termkit application.",),
             e.exception.args,
         )
