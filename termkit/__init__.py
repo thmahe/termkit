7ba99256971ba4c78714d90205d4a0d6522fab68
Thomas Mahé <contact@tmahe.dev>
2023-09-11 00:02:33 +0200

    Root __init__.py added for shorter imports

---
+++ termkit/__init__.py
@@ -0,0 +1,10 @@
+"""
+Copyright 2023, Thomas Mahé <contact@tmahe.dev>
+SPDX-License-Identifier: MIT
+
+The following imports are designed to provide convenient access to frequently used modules and functionality.
+These imports help streamline the code and improve readability by reducing the need to prefix module names.
+"""
+
+from termkit.arguments import Option, Positional  # noqa: F401
+from termkit.core import Termkit  # noqa: F401
